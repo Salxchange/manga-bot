177bf6f33c9f4c865723041227cdf0e39456036e
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-11-18 23:24:07 +0100

    Image.cbz

--- core.py
+++ core.py
@@ -1,18 +1,21 @@
+import re
+import zipfile
+from pathlib import Path
 from typing import List
 
-from telegraph.aio import Telegraph
 
-from plugins import MangaChapter
+def fld2cbz(folder: Path, name: str):
+    cbz = folder / f'{name}.cbz'
+    files = [file for file in folder.glob(r'*') if re.match(r'.*\.(jpg|png|jpeg|webp)', file.name)]
+    files.sort(key=lambda x: x.name)
+    img2cbz(files, cbz)
+    return cbz
 
 
-async def img2tph(manga_chapter: MangaChapter, name: str):
-    lines = []
-    for img in manga_chapter.pictures:
-        a_tag = f'<img src="{img}"/>'
-        lines.append(a_tag)
-    content = '\n'.join(lines)
+def img2cbz(files: List[Path], out: Path):
+    zip_file = zipfile.ZipFile(out, 'w')  # parameter "out" must be a .zip file
 
-    client = Telegraph()
-    await client.create_account('Alpha')
-    page = await client.create_page(name, author_name='AlphaBot', author_url='https://t.me/idkpythonbot', html_content=content)
-    return page['url']
+    for image_file in files:
+        zip_file.write(image_file, compress_type=zipfile.ZIP_DEFLATED)
+
+    zip_file.close()
